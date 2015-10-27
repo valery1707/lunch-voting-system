dabefd81750dcc1bf3ad96add839f4c7fbd9dd45
Valeriy.Vyrva <valery1707@gmail.com>
2015-10-27 19:45:34 +0600

    Add logging

--- src/main/java/name/valery1707/interview/lunchVote/Launcher.java
+++ src/main/java/name/valery1707/interview/lunchVote/Launcher.java
@@ -1,8 +1,15 @@
 package name.valery1707.interview.lunchVote;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.ZonedDateTime;
+
+import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
 public class Launcher {
-	
+	private static final Logger LOG = LoggerFactory.getLogger(Launcher.class);
+
 	public static void main(String[] args) {
-		System.out.println("Launcher started");
+		LOG.info("Launcher started at {}", ZonedDateTime.now().format(ISO_OFFSET_DATE_TIME));
 	}
 }
