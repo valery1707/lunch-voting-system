adca2c75923095186f5c98a8444642da58cde57a
Valeriy.Vyrva <valery1707@gmail.com>
2015-10-30 14:48:47 +0600

    Use Inject annotation

--- src/main/java/name/valery1707/interview/lunchVote/api/AccountController.java
+++ src/main/java/name/valery1707/interview/lunchVote/api/AccountController.java
@@ -1,18 +1,18 @@
 package name.valery1707.interview.lunchVote.api;
 
 import name.valery1707.interview.lunchVote.domain.Account;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
 import java.util.List;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
 
-	@Autowired
+	@Inject
 	private AccountRepo repo;
 
 	@RequestMapping(method = RequestMethod.GET)
--- src/main/java/name/valery1707/interview/lunchVote/api/StatusController.java
+++ src/main/java/name/valery1707/interview/lunchVote/api/StatusController.java
@@ -1,16 +1,17 @@
 package name.valery1707.interview.lunchVote.api;
 
 import name.valery1707.interview.lunchVote.domain.DatabaseStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.inject.Inject;
+
 @RestController
 @RequestMapping("/status")
 public class StatusController {
-	@Autowired
+	@Inject
 	private JdbcTemplate jt;
 
 	@RequestMapping(value = "/database", method = RequestMethod.GET)
