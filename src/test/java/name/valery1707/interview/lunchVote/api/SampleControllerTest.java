d2659262d9f2adc2f5ac30f42d0b6b5a981d1629
Valeriy.Vyrva <valery1707@gmail.com>
2015-10-30 15:32:32 +0600

    Add tests to project

---
+++ src/test/java/name/valery1707/interview/lunchVote/api/SampleControllerTest.java
@@ -0,0 +1,43 @@
+package name.valery1707.interview.lunchVote.api;
+
+import name.valery1707.interview.lunchVote.Launcher;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.inject.Inject;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Launcher.class)
+@WebAppConfiguration
+public class SampleControllerTest {
+	@Inject
+	private WebApplicationContext context;
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+	}
+
+	@Test
+	public void testHome() throws Exception {
+		this.mvc.perform(get("/sample"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(content().string(containsString("Hello")));
+	}
+}
