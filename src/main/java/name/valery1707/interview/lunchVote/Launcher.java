4dd587b770edb69e6880b70e67ea54cfb72734c4
Valeriy.Vyrva <valery1707@gmail.com>
2015-10-30 10:38:18 +0600

    Until this moment i trying to don't use Spring Boot AutoConfiguration because of it expensive footprint (~ 500 KiB). But adding Hibernate to the project will add about 8.6 MiB of new libraries, and 0.5 MiB of Spring_Boot_AutoConfiguration footprint seems not so big, but it will be very helpful for configure a lot of beans needed for Hibernate and Transactions working

--- src/main/java/name/valery1707/interview/lunchVote/Launcher.java
+++ src/main/java/name/valery1707/interview/lunchVote/Launcher.java
@@ -1,18 +1,11 @@
 package name.valery1707.interview.lunchVote;
 
-import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.time.ZonedDateTime;
@@ -20,32 +13,11 @@ import java.time.ZonedDateTime;
 import static java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
 @Configuration
+@EnableAutoConfiguration
 @EnableWebMvc
 @ComponentScan
 public class Launcher {
 	private static final Logger LOG = LoggerFactory.getLogger(Launcher.class);
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
-
-	@Bean
-	public EmbeddedServletContainerFactory embeddedServletContainerFactory() {
-		return new JettyEmbeddedServletContainerFactory();
-	}
-
-	@Bean
-	public DispatcherServlet dispatcherServlet() {
-		return new DispatcherServlet();
-	}
-
-	@Bean
-	public HttpMessageConverter<Object> httpMessageConverter() {
-		GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
-		converter.setGson(new GsonBuilder().setPrettyPrinting().create());
-		return converter;
-	}
 
 	public static void main(String[] args) {
 		LOG.info("Launcher started at {}", ZonedDateTime.now().format(ISO_OFFSET_DATE_TIME));
